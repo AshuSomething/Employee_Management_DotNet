09c9916996febd52eee5fd441a98aea1f04085f2
Bhargav <7386979bh@gmail.com>
2023-07-10 15:18:13 +0530

    AttanceByIdRouted

--- WebApp/Pages/Attandances/Index.cshtml.cs
+++ WebApp/Pages/Attandances/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using CoreLogic.Models;
+using CoreLogic.Services;
 using EFGetStarted;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,14 +10,24 @@ namespace WebApp.Attandances
     {
         public List<Attandance> Attendances { get; set; }
 
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+        public IActionResult OnGet()
         {
+            if (Id != 0)
+            {
+                return Redirect($"Attandances/abcd?id={Id}");
+            }
             // Load attendances for the employee
-            int employeeId = 2; // Replace with the actual employee ID
-            Attendances = GetAllAttendancesForEmployee(employeeId);
-        }
+            //int employeeId = 2; // Replace with the actual employee ID
+            Attendances = GetAllAttendancesForEmployee(Id);
 
-        private List<Attandance> GetAllAttendancesForEmployee(int employeeId)
+            
+            //return Redirect($"Attandance/abcd?id={Id}");
+            return Page();
+        }
+        
+            private List<Attandance> GetAllAttendancesForEmployee(int employeeId)
         {
             // Retrieve the attendances from your data source (database, API, etc.)
             // Replace this with your actual data retrieval logic
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +16,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
